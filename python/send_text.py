663487a97e21966522c85a1a6b7465a7e9bed0da
Candice Davidson <cndragn@gmail.com>
2017-08-06 23:41:50 -0400

    Hide auth token

--- python/send_text.py
+++ python/send_text.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC165be7ff82545c34270a64b3ac0d5f0f"
 # Your Auth Token from twilio.com/console
-auth_token  = "881bddfcf3a5343aeeb4717f67b63158"
+auth_token  = "881bddfcf3a5343aeeb4717f67b*****"
 
 client = Client(account_sid, auth_token)
 
